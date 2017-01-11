773d1551390dc36d30702b6d3639b36eb37fe708
Krista Charner <kristacharner@outlook.com>
2017-01-10 21:27:37 -0500

    appending results to page, moved on-click event to top of script code

--- javascript/app.js
+++ javascript/app.js
@@ -1,40 +1,23 @@
 
-//Gathering API data
+$("#submitBtn").on("click", function(){
+    event.preventDefault()
+    var petSearchVal = $("#userZip").val()
+    var petURL = "https://api.petfinder.com/pet.find?format=json";
+    var petKey = "&key=2cefc690d24d8a6092439aa540dd7c2d";
+    var searchLocation = "&location=" + petSearchVal; // + (unNamed var here) - need to replace 08820 with user input data - this will have to be a variable set up later and pushed to here
+    var petOutput ="&output=full"
+    var petCallBack = "&callback=?"
 
-//First we are staging our variables for the Petfinder API
-
-//These are Petfinder API arguments needed to pull data to our site:
-var petURL = "https://api.petfinder.com/pet.find?format=json";
-var petKey = "&key=2cefc690d24d8a6092439aa540dd7c2d";
-var searchLocation = "&location="; // + (unNamed var here) - need to replace 08820 with user input data - this will have to be a variable set up later and pushed to here
-var petOutput ="&output=full"
-var petCallBack = "&callback=?"
-
-//Directly below is the query URL created from variables initialized above, & where we'll need to add additional arguments related to the pets' info. petCallBack must always be last for this variable to work!
-var petQueryURL = petURL + petKey + searchLocation + petOutput + petCallBack;
-
-//WORK ON THIS TODAY - GETTING CORRECT DATA TO PUSH TO THESE VARIABLE
-
-//Conditionals for cat/dog preference will go here
-//Here I created additional variables to access later - the data contained here will be specific to the animals, and pulled from the API!
-var petStreet = "7 clinton ave" //example street
-var petCity = "edison " //example city
-var petState = "NJ" //example state
-
-//*********these next 3 lines are the start of the function I am trying to run on-click of the #submitBtn that currently isn't working
-// function petSearch() {
-//     $("#userZip").val(this).push(searchLocation);
-//     console.log(searchLocation);
-
-//sending request to Petfinder to retrieve information
-$.ajax({
-	type : 'GET',
+    var petQueryURL = petURL + petKey + searchLocation + petOutput + petCallBack;
+    
+    $.ajax({
+    type : 'GET',
     data : {},
     url : petQueryURL,
     dataType: 'json',
     success : function(response) {  
+        $("#petResponse").empty();
 
-        //we need to create a for-loop and append returned pets to page across all elements in the returned array
         var results = response.petfinder.pets.pet; //auto-pulling first 25 results?
         console.log(results);
 
@@ -68,19 +51,41 @@ $.ajax({
             //         petBreed.push(results[i].breeds.breed[x].$t);
             //         console.log(petBreed);
             //         }
-            $("#petResponse").append( "<div class='petDetes'>" + petInfo + "</div>" + "<button id='contact'><a href='modal.html'>Contact</a></button");
+        var newPetDiv = $("<div class='petDetes'>");
+        var contactBtn = $("<button id='contact'><a href='modal.html'>Contact</a></button")
+        // petResults.addClass("petDetes");
+        var petResults = newPetDiv.text(petInfo).append(contactBtn);
+        $("#petResponse").append(petResults);
+        // $("#petResponse").append(petInfo + "<button id='contact'><a href='modal.html'>Contact</a></button");
          }
-        // //Using below as test to append new div/pet details!
         },
     });
-// }; //*****this line was closing the function started above called petSearch
-//******the line below is the on-click event for calling the function
-// $("#submitBtn").on("click", petSearch());
+});
 
-//Now we are setting the stage for google Maps
+//Gathering API data
 
-//Formatting the pets' locations for google maps:
-var petLocation = petStreet + petCity + petState;
+//These are Petfinder API arguments needed to pull data to our site:
+var petURL = "https://api.petfinder.com/pet.find?format=json";
+var petKey = "&key=2cefc690d24d8a6092439aa540dd7c2d";
+var searchLocation = "&location=" + petSearchVal; // + (unNamed var here) - need to replace 08820 with user input data - this will have to be a variable set up later and pushed to here
+var petOutput ="&output=full"
+var petCallBack = "&callback=?"
+
+//Directly below is the query URL created from variables initialized above, & where we'll need to add additional arguments related to the pets' info. petCallBack must always be last for this variable to work!
+var petQueryURL = petURL + petKey + searchLocation + petOutput + petCallBack;
+
+//WORK ON THIS TODAY - GETTING CORRECT DATA TO PUSH TO THESE VARIABLE
+
+//Conditionals for cat/dog preference will go here
+//Here I created additional variables to access later - the data contained here will be specific to the animals, and pulled from the API!
+var petStreet = "7 clinton ave" //example street
+var petCity = "edison " //example city
+var petState = "NJ" //example state
+
+// //Now we are setting the stage for google Maps
+
+// //Formatting the pets' locations for google maps:
+// var petLocation = petStreet + petCity + petState;
 
 //google Maps API arguments for displaying map and placing a location pin
 var mapsURL = "https://maps.googleapis.com/maps/api/geocode/json?";
