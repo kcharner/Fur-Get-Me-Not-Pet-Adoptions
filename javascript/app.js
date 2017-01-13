4161acefefd23ccade52eded6422773423eea699
Krista Charner <kristacharner@outlook.com>
2017-01-12 20:12:48 -0500

    updated js w/ last-update sorting

--- javascript/app.js
+++ javascript/app.js
@@ -5,31 +5,24 @@ $("#submitBtn").on("click", function(){
     $("#userPetSearch").hide();
     $('#petResponse').show();
 
-    var petStreet;
-    var petCity;
-    var petState;
-    var petFullAddress;
-    var petSearchType;
-    var petSearchGen;
-    var petQueryURL;
-    var petSearchVal = $("#userZip").val();
+   var petStreet, petCity, petState, petFullAddress, petSearchGen, petSearchType, petQueryURL;
+   var petSearchVal = $("#userZip").val();
 
-    if ($("#petType")["0"][0].selected == true) {
+   if ($("#petType")["0"][0].selected == true) {
         petSearchType = "&animal=cat";
-    }
+   }
 
-    else if ($("#petType")[0].selected == true) {
+   else if ($("#petType")["0"][1].selected == true) {
         petSearchType = "&animal=dog";
-    }
+   }
 
-    else {
+   else {
         petSearchType = undefined;
-    }
+   }
 
-
-    if ($('#gender')["0"][0].selected == true) {
+   if ($('#gender')["0"][0].selected == true) {
             petSearchGen = "&sex=F"
-    }
+   }
 
     else if ($('#gender')["0"][1].selected == true) {
             petSearchGen = "&sex=M"
@@ -44,79 +37,71 @@ $("#submitBtn").on("click", function(){
     var searchLocation = "&location=" + petSearchVal ;
     var petOutput ="&output=full&callback=?"
 
-    if (petSearchGen == undefined && petSearchType == undefined) {
-        petQueryURL = petURL + petKey + searchLocation + petOutput ;
-    }
+if (petSearchGen == undefined && petSearchType == undefined) {
+    petQueryURL = petURL + petKey + searchLocation + petOutput ;
+}
 
-    else if (petSearchGen != undefined && petSearchType == undefined) {
-        petQueryURL = petURL + petKey + searchLocation + petSearchGen + petOutput;
-    }
+else if (petSearchGen != undefined && petSearchType == undefined) {
+    petQueryURL = petURL + petKey + searchLocation + petSearchGen + petOutput;
+}
 
-    else if (petSearchGen == undefined && petSearchType != undefined) {
-        petQueryURL = petURL + petKey + searchLocation + petSearchType + petOutput;
-    }
+else if (petSearchGen == undefined && petSearchType != undefined) {
+    petQueryURL = petURL + petKey + searchLocation + petSearchType + petOutput;
+}
 
-    else {
-        petQueryURL = petURL + petKey + searchLocation + petSearchGen + petSearchType + petOutput;
-    }
+else {
+    petQueryURL = petURL + petKey + searchLocation + petSearchGen + petSearchType + petOutput;
+}
+
 
     $.ajax({
-        type: 'GET',
-        data: {},
-        url: petQueryURL,
+        type : 'GET',
+        data : {},
+        url : petQueryURL,
         dataType: 'json',
-        success: function(response) {  
+        success : function(response) {  
                  $("#petResponse").empty();
 
                 var results = response.petfinder.pets.pet;
-                console.log(results)
+                
+                var resultsPreSorted = []
+                    results.forEach(function(petData){
+                        var petSorted= {};
+                            petSorted.petName = petData.name.$t;
+                            petSorted.petAge = petData.age.$t;
+                            petSorted.petType = petData.animal.$t;
+                            petSorted.petGender = petData.sex.$t;
+                            petSorted.petInfo = petSorted.petName + " " + petSorted.petAge + " " + petSorted.petType + " " + petSorted.petGender;
+                       
+                           
+                            petSorted.petStreet = petData.contact.address1.$t;
+                            petSorted.petCity = petData.contact.city.$t;
+                            petSorted.petState = petData.contact.state.$t;
+                            petSorted.petFullAddress =   petSorted.petStreet + ",+" +  petSorted.petCity + ",+" + petSorted.petState ;
+                            
 
-                for (var i = 0; i < results.length; i++) {
-                    var petName = results[i].name.$t;
-                    var petAge = results[i].age.$t;
-                    var petType = results[i].animal.$t;
-                    var petGender = results[i].sex.$t;
-                    var petInfo = petName + " " + petAge + " " + petType + " " + petGender;
+                            petSorted.petEmail = petData.contact.email.$t;
+                            petSorted.petPhone = petData.contact.phone.$t;
+                            petSorted.petDescription = petData.description.$t;
+                            petSorted.petLastUpdate = petData.lastUpdate.$t.split("T");
+                            var petDate = petSorted.petLastUpdate
+                            petSorted.updateTimeEpoch =  Math.floor(moment(petDate).valueOf());
+                            petSorted.petPics = petData.media.photos.photo[0].$t;
+                            
+                            resultsPreSorted.push(petSorted);    
+                }); // end for each loop
+            var resultSorted = resultsPreSorted.sort(function(a, b){return a.updateTimeEpoch-b.updateTimeEpoch});
+              console.log(resultSorted)
+           for (var i = 0; i  < resultSorted.length; i++) {
                
-                   
-                    petStreet = results[i].contact.address1.$t;
-                    petCity = results[i].contact.city.$t;
-                    petState = results[i].contact.state.$t;
-                    petFullAddress =  petStreet + ",+" + petCity + ",+" + petState;
+                 var petContainer = $("<div class='col-md-3 fourAcross'>")
+                 var newPetDiv = $("<div class='petDetes'>");
+                 var petImage = $("<img class='imagez'>").attr("src", resultSorted[i].petPics).attr("data-address", resultSorted[i].petFullAddress);
                     
-
-                    var petEmail = results[i].contact.email.$t;
-                    var petPhone = results[i].contact.phone.$t;
-                    var petDescription = results[i].description.$t;
-                    var petLastUpdate = results[i].lastUpdate.$t;
-
-                                        //     for (var y = 0; y < results[i].media.photos.photo.length; y++) {
-                                        //         petPics.push(results[i].media.photos.photo[y].$t);
-                                        //         console.log(petPics);
-                                        //         }
-                                        // var petDetails = [];
-                                        //     for (var z = 0; z < results[i].options.option.length; z++) {
-                                        //         petDetails.push(results[i].options.option[z].$t);
-                                        //         console.log(petDetails);
-                                        //         }
-                                        // var petBreed = [];
-                                        // console.log(petName + " " + petAge);
-                                        //     for (var x = 0; x < results[i].breeds.breed.length; x++) {
-                                        //         petBreed.push(results[i].breeds.breed[x].$t);
-                                        //         console.log(petBreed);
-                                        //         }
-
-                    var petPics = results[i].media.photos.photo[0].$t;
-
-                    var petContainer = $("<div class='col-md-3 fourAcross'>")
-                    var newPetDiv = $("<div class='petDetes'>");
-                    var petImage = $("<img class='imagez'>").attr("src", petPics).attr("data-address", petFullAddress);
-                    
-                    petContainer.append(newPetDiv).append(petImage);
-                    var contactBtn = $("<button id='contact'><a href='modal.html'>Contact</a></button");
-                    var petResults = newPetDiv.text(petInfo).append(contactBtn);
-                    $("#petResponse").append(petContainer);      
-             } // For loop
+                 petContainer.append(newPetDiv).append(petImage);
+                 var petResults = newPetDiv.text(resultSorted[i].petInfo);
+                 $("#petResponse").append(petContainer);
+       }
         }, //AJAX function
     }); // AJAX Request
 
@@ -172,3 +157,6 @@ function markerMap() {
       }
 
 
+                    
+
+    
